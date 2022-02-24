262728f2a9a3475b624963daf8e416be9a432520
Casey Ocampo <caseysoflo@gmail.com>
2022-02-24 12:02:43 -0500

    Add async await

--- index.js
+++ index.js
@@ -8,55 +8,52 @@ const winnerTextDisplay = document.getElementById('winnerTextDisplay')
 const computerScoreDisplay = document.getElementById('computerScoreDisplay')
 const userScoreDisplay = document.getElementById('userScoreDisplay')
 
-function handleClick() {
-    fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            deckId = data.deck_id
-            drawCardsBtn.classList.remove('disabled')
-            cardsRemaining.textContent = `Cards remaining: ${data.remaining}`
-            cardsRemaining.style.visibility = 'visible'
-            drawCardsBtn.disabled = false
-        })
+async function handleClick() {
+    // prettier-ignore
+    const res = await fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
+    const data = await res.json()
+    deckId = data.deck_id
+    cardsRemaining.textContent = `Cards remaining: ${data.remaining}`
+    cardsRemaining.style.visibility = 'visible'
+    drawCardsBtn.disabled = false
+    drawCardsBtn.classList.remove('disabled')
 }
 
 newDeckBtn.addEventListener('click', handleClick)
 
-drawCardsBtn.addEventListener('click', () => {
-    fetch(
+drawCardsBtn.addEventListener('click', async () => {
+    const res = await fetch(
         `https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`
     )
-        .then(res => res.json())
-        .then(data => {
-            cards.children[1].innerHTML = `
+    const data = await res.json()
+
+    cards.children[1].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
 
-            cards.children[2].innerHTML = `
+    cards.children[2].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
 
-            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
-            winnerTextDisplay.textContent = winnerText
-            cardsRemaining.textContent = `Cards remaining: ${data.remaining}`
+    const winnerText = determineCardWinner(data.cards[0], data.cards[1])
+    winnerTextDisplay.textContent = winnerText
+    cardsRemaining.textContent = `Cards remaining: ${data.remaining}`
 
-            if (data.remaining === 0) {
-                drawCardsBtn.disabled = true
-                drawCardsBtn.classList.add('disabled')
-                if (computerScore > userScore) {
-                    winnerTextDisplay.textContent = 'Computer wins the game!'
-                } else if (computerScore < userScore) {
-                    winnerTextDisplay.textContent = 'You win the game!'
-                } else if (computerScore === userScore) {
-                    winnerTextDisplay.textContent = `It's a tie game!`
-                }
-            }
+    if (data.remaining === 0) {
+        drawCardsBtn.disabled = true
+        drawCardsBtn.classList.add('disabled')
+        if (computerScore > userScore) {
+            winnerTextDisplay.textContent = 'Computer wins the game!'
+        } else if (computerScore < userScore) {
+            winnerTextDisplay.textContent = 'You win the game!'
+        } else if (computerScore === userScore) {
+            winnerTextDisplay.textContent = `It's a tie game!`
+        }
+    }
 
-            for (let card of cardSlot) {
-                card.style.border = 'none'
-            }
-        })
+    for (let card of cardSlot) {
+        card.style.border = 'none'
+    }
 })
 
 let computerScore = 0
